85bbcada067d847686c1fd3a649861e142fe8dcf
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-05-19 10:17:06 +0200

    Added basic Webpack fuctionality

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+
+console.log("Dai GIovanni, chisle away!!!")
+
+const element = document.createElement("h1")
+element.innerHTML = "Forza Giovanni, vai che te ghe si!!!"
+document.body.appendChild(element)  
+
+
+
+// 5) Transfer all to Webpack
+// 1) Create event listener
+// 2) Create a URL builder
+// 3) fetch API via axios
+// 4) Print Data
+
